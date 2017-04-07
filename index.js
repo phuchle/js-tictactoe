5a7e5d6c315cbc4dfaa37fabc51f1f8ea14e944b
Phuc Le <le.phuc.h@gmail.com>
2017-04-07 16:19:00 -0700

    Add function to make the marks show up on the board when squares are clicked.

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 // player can make a mark on the board corresponding to their symbol
 // game ends when there are three of same mark in a row or all squares filled
+var game;
+var player;
 
 class Player {
-  constructor(symbol) {
-    this.symbol = symbol;
-  }
-
   move(square) {
-    square.innerText = this.symbol;
+    square.innerHTML = this.symbol;
   }
 }
 
@@ -15,46 +13,55 @@ class Board {
   constructor() {
     this.board = []; // 1D array of squares div
 
-    // Add each square to this.board
+    // Add each square to this.board to gain access to Array methods
     Array.prototype.forEach
       .call(document.getElementsByClassName('squares'), item => {
         this.board.push(item);
       }
     );
 
-    // 2D array of rows
+    // 2D array of board rows
     this.rows = [
       this.board.slice(0, 3),
       this.board.slice(3, 6),
       this.board.slice(6)
     ]
 
-    // 2D array of the columns
+    // 2D array of the board columns
     this.columns = [
       this.board.filter(square => { return square.className.slice(-1) === '0' }),
       this.board.filter(square => { return square.className.slice(-1) === '1' }),
       this.board.filter(square => { return square.className.slice(-1) === '2' })
     ]
+    // 2D array of the board diagonals
     this.diagonals = [
       [this.board[0],this.board[4],this.board[8]],
       [this.board[2],this.board[4],this.board[6]]
     ]
-    console.log(this.diagonals);
+
   }
 
+  addListeners() {
+    this.board.forEach(square => {
+      square.addEventListener('click', () => {
+        player.move(square);
+      });
+    });
+  }
 }
+
 
 var chooseSymbol = () => {
   var symbolContainer = document.getElementById('symbol-container');
   var x = document.getElementById('X');
   var o = document.getElementById('O');
-  var player;
   var symbols = [x,o];
 
   symbols.forEach(symbol => {
     symbol.addEventListener('click', () => {
-      player = new Player(symbol.innerText);
       symbolContainer.style.opacity = 0;
+      player.symbol = symbol.innerText;
+      game.addListeners();
       showBoard();
       showInstructions();
     });
@@ -72,6 +79,7 @@ var showInstructions = () => {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
+  player = new Player();
+  game = new Board();
   chooseSymbol();
-  var game = new Board();
 });
